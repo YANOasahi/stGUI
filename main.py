b2bed630a9d388292f9637b0433e8d8223cb86d2
root <root@yano-laptop.>
2024-11-01 23:10:15 +0900

    2024.11.1 23:07(JST)

--- main.py
+++ main.py
@@ -1,4 +1,8 @@
-# import modules
 import knobs
+import OneSec
+import tkinter as tk
 
+# before opening the window, run onesec()
+knobs.window.after(10, OneSec.onesec)
+# open window
 knobs.window.mainloop()
