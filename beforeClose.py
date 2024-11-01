dd4a55025126c280768252eeff39509acb7f5843
root <root@yano-laptop.>
2024-11-02 04:23:48 +0900

    add inter lock when we click the close button

---
+++ beforeClose.py
@@ -0,0 +1,28 @@
+def before_close():
+    import variables
+    import activate
+    import knobs
+    import tkinter as tk
+    import paramiko  # type: ignore
+
+    if variables.flagSSH0 == 0:
+        try:
+            # read current from power supply
+            stdin, stdout, stderr = activate.ssh0.exec_command('meas:curr?')
+            read0_outpA = stdout.read().decode()+stderr.read().decode()
+            if read0_outpA == '0.0':
+                knobs.window.destroy()
+            else:
+                noticeLabel1 = tk.Label(knobs.window, text='You can\'t close window now.', font=(
+                    "Arial", 16), bg='red', relief=tk.RIDGE).place(x=10, y=540)
+                noticeLabel2 = tk.Label(knobs.window, text='Make current 0 A first.', font=(
+                    "Arial", 16), bg='red', relief=tk.RIDGE).place(x=10, y=570)
+                pass
+        except Exception as e:
+            noticeLabel1 = tk.Label(knobs.window, text='Check connection.', font=(
+                "Arial", 16), bg='red', relief=tk.RIDGE).place(x=10, y=540)
+            pass
+    else:
+        noticeLabel1 = tk.Label(knobs.window, text='Check connection.', font=(
+            "Arial", 16), bg='red', relief=tk.RIDGE).place(x=10, y=540)
+        pass
--- main.py
+++ main.py
@@ -1,7 +1,9 @@
 import knobs
 import OneSec
 import tkinter as tk
+import beforeClose
 
+knobs.window.protocol("WM_DELETE_WINDOW", beforeClose.before_close)
 # before opening the window, run onesec()
 knobs.window.after(10, OneSec.onesec)
 # open window
