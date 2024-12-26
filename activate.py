92780bf17f9e0b12d1a551521161a64a0dde579d
YANOasahi <r3-lenovoe14@R3-LenovoE14.>
2024-12-26 17:31:41 +0900

    not ok

--- activate.py
+++ activate.py
@@ -1,32 +1,33 @@
 def activate0():
     import tkinter as tk
     import variables
-    import knobs
     import subprocess
+    import connect_ssh
 
-    if variables.flagSSH0 == 1:
-        return
+    connect_ssh.connect_ssh0()
 
-    try:
-        answer = subprocess.run(['addr','1'])
-        print(answer)
-        variables.flagActivate0 = 0
-    except Exception as e:
-        variables.flagActivate0 = 1
+    if variables.flagSSH0 == 0:
+        st1_process.stdin.write(['addr','1'])
+        answer = st1_process.stdout.read()
+        if answer == 'OK':
+            variables.flagActivate0 = 0
+        else:
+            variables.flagActivate0 = 1
 
 
 def activate1():
     import tkinter as tk
     import variables
-    import knobs
     import subprocess
+    import connect_ssh
 
-    if variables.flagSSH1 == 1:
-        return
+    connect_ssh.connect_ssh1()
 
-    try:
-        answer = subprocess.run(['addr','1'])
-        print(answer)
-        variables.flagActivate1 = 0
-    except Exception as e:
-        variables.flagActivate1 = 1
+    if variables.flagSSH1 == 0:
+        st2_process.stdin.write(['addr','1'])
+        answer = st2_process.stdout.read()
+        if answer == 'OK':
+            print(answer)
+            variables.flagActivate1 = 0
+        else:
+            variables.flagActivate1 = 1
--- connect_ssh.py
+++ connect_ssh.py
@@ -1,26 +1,26 @@
 def connect_ssh0():
     import subprocess
     import variables
-
-    try:
-        subprocess.run(['socat','-',f'TCP:{variables.ip[0]}:{variables.port[0]}'])
+    
+    st1_process = subprocess.Popen(['socat','-',f'TCP:{variables.ip[0]}:{variables.port[0]},connect-timeout=3'])
+    print(st1_process)
+    if st1_process.returncode==0:
         print(f'connected to {variables.ip[0]}:{variables.port[0]}')
         variables.flagSSH0 = 0
-    except Exception as e:
+    else:
         print('failed to connect...')
         variables.flagSSH0 = 1
-        return str(e)
-
+       
 
 def connect_ssh1():
     import subprocess
     import variables
 
-    try:
-        subprocess.run(['socat','-',f'TCP:{variables.ip[1]}:{variables.port[1]}'])
+    st2_process = subprocess.Popen(['socat','-',f'TCP:{variables.ip[1]}:{variables.port[1]},connect-timeout=3'])
+    print(st2_process)
+    if st2_process.returncode==0:
         print(f'connected to {variables.ip[1]}:{variables.port[1]}')
         variables.flagSSH1 = 0
-    except Exception as e:
+    else:
         print('failed to connect...')
         variables.flagSSH1 = 1
-        return str(e)
